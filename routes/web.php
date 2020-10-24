ad78c635a9fc827660abfdd3230c4dcf08c5ba37
yudai <negi4028@gmail.com>
2020-10-24 10:30:17 +0000

    third commit

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::group(['prefix' => 'admin'], function() {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
