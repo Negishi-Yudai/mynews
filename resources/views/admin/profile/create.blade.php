d8f29d94b9ef7e678da15bdb25b65c809aea19c7
yudai <negi4028@gmail.com>
2020-10-27 05:02:29 +0000

    second commit

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MY Profile </title>
+    </head>
+    <body>
+        <h1>MYプロフィール</h1>
+        {{-- layouts/profile.blade.phpを読み込む --}}
+         @extends('layouts.profile')
+
+    </body>
+</html>
+
